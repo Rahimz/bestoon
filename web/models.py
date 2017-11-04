2026d17d4b8012bd705e8340791b7786656538ce
rahimz <rahim.aghareb@gmail.com>
2017-11-04 16:12:10 +0330

    income added

--- web/models.py
+++ web/models.py
@@ -6,3 +6,8 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
