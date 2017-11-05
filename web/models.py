4abe74306f992e0a7781291bd2ad4899168e5876
rahimz <rahim.aghareb@gmail.com>
2017-11-05 19:05:45 +0330

    clean somthing

--- web/models.py
+++ web/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -6,8 +8,19 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+    # this function return a good name for expense objects in admin site
+    def __str__(self):
+        return "{} - {}".format(str(self.date), str(self.amount))
+        # return "{}-{}".format(self.date, self.amount)
+
+
+
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{} - {}".format(str(self.date), str(self.amount))
