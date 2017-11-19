7ddff62bf5f395e1f8101e47f27ec52070b723d5
rahimz <rahim.aghareb@gmail.com>
2017-11-19 23:28:00 +0330

    submit/expense and submit/income web servises

--- web/views.py
+++ web/views.py
@@ -9,6 +9,30 @@ from datetime import datetime
 # Create your views here.
 
 @csrf_exempt
+def submit_income(request):
+    """user submits an income"""
+
+
+    #TODO validate might be fake user might be fake, token might be fake
+    this_token = request.POST['token'] if 'token' in request.POST else "1234567"
+    this_user = User.objects.filter(token__token = this_token).get()
+    this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
+    this_text = request.POST['text'] if 'text' in request.POST else ""
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Income.objects.create(user = this_user, amount=this_amount,
+        text = this_text, date=date)
+
+
+
+    return JsonResponse({
+        'status': 'ok',
+
+    }, encoder=JSONEncoder)
+
+
+
+@csrf_exempt
 def submit_expense(request):
     """user submits an expense"""
 
